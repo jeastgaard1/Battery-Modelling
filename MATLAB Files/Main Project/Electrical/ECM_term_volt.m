12032bc1dbfcd535ed8f31c6140850118dec2294
jeastgaard1 <josheastgaard@gmail.com>
2026-02-06 07:35:43 +0100

    Updated comments in code. Added Modeling File.

--- MATLAB Files/Main Project/Electrical/ECM_term_volt.m
+++ MATLAB Files/Main Project/Electrical/ECM_term_volt.m
@@ -5,10 +5,9 @@ function U_term = ECM_term_volt(t, U_RC, param)
 U_RC1 = U_RC(1);
 U_RC2 = U_RC(2);
 
-if t <= param.time_mid
-    U_term = param.UCell(t) - param.DCH_I * param.R0(t) - U_RC1 - U_RC2;
-else
-    U_term = param.UCell(t) - param.CH_I * param.R0(t) - U_RC1 - U_RC2;
-end
+I = param.I(t);
+
+U_term = param.UCell(t) - I * param.R0(t) - U_RC1 - U_RC2;
+
 
 end
